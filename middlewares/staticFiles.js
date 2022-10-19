1a88b514e4d26aca6a84ebfdfbde09c0dc04a50c
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-10-19 10:16:11 +0200

    - Test - named export static

--- middlewares/staticFiles.js
+++ middlewares/staticFiles.js
@@ -1,9 +1,9 @@
-import express from "express";
+import {static as staticFiles} from "express";
 import path from "path";
 import {staticFilesRelativePath} from "../configs/application.js";
 
 const staticFilesPath = path.resolve(staticFilesRelativePath);
 
-export default function (app) {
-    app.use(express.static(staticFilesPath));
+export default function (app){
+    app.use(staticFiles(staticFilesPath));
 }
