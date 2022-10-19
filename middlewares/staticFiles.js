daff68dece81e7bb5f4fb030f1c966716115e12f
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-10-19 09:57:21 +0200

    - Changed - express static files import.

--- middlewares/staticFiles.js
+++ middlewares/staticFiles.js
@@ -1,9 +1,9 @@
-import {static as staticFiles} from "express";
+import express from "express";
 import path from "path";
 import {staticFilesRelativePath} from "../configs/application.js";
 
 const staticFilesPath = path.resolve(staticFilesRelativePath);
 
-export default function (app){
-    app.use(staticFiles(staticFilesPath));
+export default function (app) {
+    app.use(express.static(staticFilesPath));
 }
