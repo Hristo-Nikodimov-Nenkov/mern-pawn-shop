7342dd44c78c3ff54e51939bc01b48fe02c5d4f0
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-10-20 13:15:05 +0200

    - Removed - dummy content.

--- client/src/components/pages/Home/Items/Items.js
+++ client/src/components/pages/Home/Items/Items.js
@@ -1,33 +1,5 @@
-import Item from "./index";
-
 const Items = () => {
-    const items = [
-        {
-            id: 1,
-            name: "Item 1",
-            description: "The item 1 description",
-            price: 0.50
-        },
-        {
-            id: 2,
-            name: "Item 2",
-            description: "The item 2 description",
-            price: 0.90
-        },
-        {
-            id: 3,
-            name: "Item 3",
-            description: "The item 3 description",
-            price: 0.78
-        }
-    ]
-
-    const mappedItems = items.map(i => <Item key={i.id} item={i}/>)
-
-    return <>
-        <h2>List of items</h2>
-        {mappedItems}
-    </>
+    return <h2>List of items</h2>
 }
 
 export default Items;
