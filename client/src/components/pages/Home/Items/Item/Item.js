b3f88063a591cfd2d39d9cc19e722facc4eec806
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-10-20 16:11:12 +0200

    - Changed - Item component.

--- client/src/components/pages/Home/Items/Item/Item.js
+++ client/src/components/pages/Home/Items/Item/Item.js
@@ -1,5 +1,9 @@
 const Item = ({item}) => {
-    return <h3>Item {item.name}</h3>
+    return <>
+        <h3>Item {item.name}</h3>
+        <div>{item.description}</div>
+        <div>Price ${item.price}</div>
+    </>
 }
 
 export default Item;
