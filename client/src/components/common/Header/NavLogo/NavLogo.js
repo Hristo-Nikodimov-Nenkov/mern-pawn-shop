be839d4aa77635cc600debb072640176d45e0b82
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-10-21 10:45:27 +0200

    - Changed logo styles - Changed logo image

--- client/src/components/common/Header/NavLogo/NavLogo.js
+++ client/src/components/common/Header/NavLogo/NavLogo.js
@@ -1,6 +1,8 @@
+import "./NavLogo.css";
+
 const NavLogo = () => {
     return <div className="header-logo-container">
-        <img className="header-logo-image" alt="PawnShop logo."/>
+        <img src="/logo.png" className="header-logo-image" alt="PawnShop logo."/>
     </div>
 }
 
